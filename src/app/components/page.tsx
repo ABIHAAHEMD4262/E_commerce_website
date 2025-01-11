85d6dc12205c377687819e09a205f0b1bd8d509f
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2025-01-11 23:09:01 +0500

    Initial commit from Create Next App

---
+++ src/app/components/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface User{
+    name:string,
+    price:number,
+    image:string,
+    id:string
+}
+const page = () => {
+
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
